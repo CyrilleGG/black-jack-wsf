c63d70bb846249f6379511cb7e6806968ca060ac
Cyrille Guiot-Guillin <cyrille.guiot-guillin@etu-webschoolfactory.fr>
2018-02-12 18:32:44 +0100

    v 0.1.0
    Définition du deck + système de pioche

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 var cards: [[String: [Any]]] = [          //les index (couleurs) sont en optionnels => ne pas oublier à les unwrapper
-    ["Heart": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]],
-    ["Diamond": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]],
-    ["Club": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]],
-    ["Spade": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]]
+    ["Hearts": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]],
+    ["Diamonds": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]],
+    ["Clubs": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]],
+    ["Spades": [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]]
 ]
 
 
@@ -15,11 +15,10 @@ var cards: [[String: [Any]]] = [          //les index (couleurs) sont en optionn
 
 
 let chooseColor = Int(arc4random_uniform(4))
+let chooseCard = Int(arc4random_uniform(13))
 
 let couleur = cards[chooseColor]
 
-print(couleur)
-
-for(key, _) in couleur {
-    print("\(key)")
+for(key, value) in couleur {
+    print("\(value[chooseCard])" + " of " + "\(key)")
 }
